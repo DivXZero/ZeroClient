c0ff2d2245c2d463c4a4efaf58578c306afe0cf7
Nick <silv3rlogic@gmail.com>
2015-03-05 15:48:33 -0800

    modified script

--- hello.rb
+++ hello.rb
@@ -11,26 +11,18 @@ class MyTest < Test
   
   def update
     @update_count+=1
-	if((@update_count % 1000000) == 0)
-	  $time = Time.now - $time
-	  $avg += $time
-	  puts "#{(@update_count / 100000)}% : #{$time} sec"
+	$frameCount+=1
+	if((Time.now - $time) >= 1)
+	  print "#{$frameCount} updates per second.            \r"
+	  $frameCount = 0;
 	  $time = Time.now
-	end
-	
-	if(@update_count == 10000000)
-	  $avg = $avg / 10
-	  perSec = (1 / $avg) * 1000000
-	  puts "Results: averaged #{perSec.floor} updates per second.                        "
-	  $avg = 0
-	  @update_count = 0
 	end
   end
 end
 
 $testObj = MyTest.new
 $time = Time.now
-$avg = 0
+$frameCount = 0
 
 def update
   $testObj.update
